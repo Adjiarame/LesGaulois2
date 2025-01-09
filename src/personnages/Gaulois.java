2b6e1187384db917f971b07ad4dbebed334c9df7
Adjiarame <adji-aram.seck@univ-tlse3.fr>
2025-01-09 09:53:12 +0100

    fin scéance 1

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,5 +1,29 @@
 package personnages;
 
 public class Gaulois {
+	private String nom;
+	private int force;
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	public String getGaulois() {
+		return nom;
+	}
+	
+	public int getForce() {
+		return force;
+	}
+	
+	public static void main (String[] args) {
+		
+	      Gaulois asterix = new Gaulois("Astérix", 8);
+		
+		  System.out.println(asterix.getGaulois());
 
+	}
+	
+	public void parler(String chaine) {
+		System.out.println("Le gaulois " + nom + " : \"" +  chaine  + "\".");
+	}
 }
